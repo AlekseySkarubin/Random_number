9e689f0de1609f22cff003afd8551b79d0240a21
Aleksey <142098143+AlekseySkarubin@users.noreply.github.com>
2025-03-29 23:00:29 +0300

    Update script.js
    Возвращаемся на версию при которой работало отображение чисел

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function numberToText(n) {
     const units = ["", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"];
     const teens = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемьнадцать", "девятнадцать"];
     const tens = ["", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"];
+    const hundreds = ["", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"];
 
     if (n === 0) return "ноль";
 
@@ -11,9 +12,13 @@ function numberToText(n) {
         n = -n;
     }
 
+    let hundred = Math.floor(n / 100);
     let ten = Math.floor((n % 100) / 10);
     let unit = n % 10;
 
+    if (hundred > 0) {
+        result += hundreds[hundred] + " ";
+    }
     if (ten === 1) {
         result += teens[unit];
     } else {
@@ -74,11 +79,8 @@ function updateQuestion() {
     const randomPhrase = questionPhrases[Math.floor(Math.random() * questionPhrases.length)];
 
     const answerText = numberToText(answerNumber);
-    const fullQuestion = `${randomPhrase} ${answerText}?`;
-
-    // Проверяем длину только текстового представления числа
     if (answerText.length < 20) {
-        document.getElementById('answerField').innerText = fullQuestion;
+        document.getElementById('answerField').innerText = `${randomPhrase} ${answerText}?`;
     } else {
         document.getElementById('answerField').innerText = `${randomPhrase} ${answerNumber}?`;
     }
